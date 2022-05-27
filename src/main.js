131dd92a97d54b0643f5db01209e517a3b4e5cff
AlekseySamokhin <lel.dude01@gmail.com>
2022-05-27 11:07:16 +0300

    refactor: refactoring app

--- src/main.js
+++ src/main.js
@@ -1,18 +1,21 @@
-import React from "react";
-import ReactDom from "react-dom";
+import React from 'react'
+import ReactDom from 'react-dom';
 
-import Counter from "./counter/index.js";
+import App from './App';
 
 ReactDom.render(
-	<div className="some">
-		<h2>Сounter</h2>
-		<hr />
-		<h3>values: min=1 max=5</h3>
-		<hr />
-		<Counter min={1} max={10} />
-		<hr />
-		<Counter min={0} max={100} />
-		<hr />
-	</div>,
-	document.querySelector(".app"),
+	<App/>,
+	document.querySelector('.app')
 );
+
+/*
+	useState
+	useEffect
+	useCallback
+	useMemo
+	useReducer
+	useContext
+	useRef
+*/
+
+
